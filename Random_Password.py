bd699e8ae6ec0730e6331d1737100443dd7e206b
HAWK <Hawk3828@gmail.com>
2020-05-17 00:52:37 -0400

    Changes to random generator for example

--- Random_Password.py
+++ Random_Password.py
@@ -1,27 +1,31 @@
 '''
 Created on May 16, 2020
-
 @author: Hawkins Jean
+worked with Nathan on this code
 '''
-
+# Declare your API...programming interfaces
 import random
 import string
 
+
 LETTERS = string.ascii_letters
 NUMBERS = string.digits
+AUX_Numbers = string.octdigits
 PUNCTUATION = string.punctuation
 
 
-print ('Use Character below to generatre a string password\n')
+print ('Use Character below to generate a string password\n')
 print( LETTERS )
 print( NUMBERS )
+print( AUX_Numbers )
 print( PUNCTUATION )
 
+# New Line
 print('\n')
 
 
 
-# Create function to shuffle the caharacter fo the string
+# Create function to shuffle the characters for the string
 def crossover(string):
     tempList = list(string)
     random.shuffle(tempList)
@@ -40,7 +44,7 @@ puncutationSign2=chr(random.randint(35,38))
 
 password = uppercaseLetter1 + uppercaseLetter2 + lowercaseLetter1 + lowercaseLetter2 + digit1 + digit2 + punctuationSign1 + puncutationSign2     
 
-# Call the corssover function
+# Call the crossover function
 password = crossover(password)
 
 print( 'Here it is:  ' + password )
